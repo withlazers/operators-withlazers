b84346e6e2647d4b5734176a257d13771c08e6d1
Enno Boland <g@s01.de>
2022-06-30 20:42:02 +0200

    common: update

--- common/src/prelude.rs
+++ common/src/prelude.rs
@@ -10,9 +10,10 @@ pub use kube::{
     api::{ListParams, PostParams},
     core::ObjectMeta,
     runtime::{controller::Action, Controller},
-    Api, Client, ResourceExt,
+    Api, Client, CustomResource, ResourceExt,
 };
 pub use log::*;
 pub use pretty_env_logger::init as init_logger;
+pub use serde::{Deserialize, Serialize};
 pub use thiserror;
 pub use tokio;
